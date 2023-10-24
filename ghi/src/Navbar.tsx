51b1ab69825e749a02ce154a36febc5e073a6b11
Joseph Pinkus-Lock <joelock3@hotmail.com>
2023-10-24 13:44:22 -0400

    navbar css and component updates

--- ghi/src/Navbar.tsx
+++ ghi/src/Navbar.tsx
@@ -1,34 +1,97 @@
-import React, { useEffect, useState, useContext} from "react";
+import React, { useEffect, useState, useContext, useRef} from "react";
 import { NavLink } from "react-router-dom";
+
 
 function Navbar() {
 
+    interface dropDownMenu {
+        show: boolean;
+        section: string
+    }
+    
+    const navBar = React.useRef<HTMLDivElement>(null);
+
+    useOutsideAlerter(navBar)
+
     const [showMenu, setShowMenu] = useState(false);
+    const [showDropdownMenu, setShowDropdownMenu] = useState<dropDownMenu>({
+        show: false,
+        section: ""
+     });
 
     const handleShowMenu = () => {
         setShowMenu(!showMenu);
     }
 
+    const handleShowDropdownMenu = (show: boolean, section: string) => {
+        setShowDropdownMenu({show: show, section: section});
+    }
+
+    function useOutsideAlerter(ref: React.RefObject<HTMLDivElement | null>) {
+        useEffect(() => {
+          // Function for click event
+          function handleOutsideClick(event: MouseEvent) {
+            if (ref.current && !ref.current.contains(event.target as Node)) {
+              handleShowDropdownMenu(false, "none");
+            }
+          }
+          // Adding click event listener
+          document.addEventListener("click", handleOutsideClick);
+          return () => document.removeEventListener("click", handleOutsideClick);
+        }, [ref]);
+      }
+
+    useEffect(() => {
+        function handleResize() {
+          if (window.innerWidth > 800 && showMenu) {
+            // If window size is >= 800px and menu is open, close the menu
+            setShowMenu(false);
+          }
+        }
+    
+        window.addEventListener('resize', handleResize);
+    
+        return () => {
+          window.removeEventListener('resize', handleResize);
+        };
+      }, [showMenu]);
+
     return (
-        <nav className="navbar">
+        <nav className="navbar" ref={navBar}>
             <div className="navmain">
                 <div className="nav-header">
-                    <img src="bored-game-logo.svg" alt="logo" />
+                    <img src="bored-game-logo.svg" alt="logo"/>
                     <i className="fa fa-bars" aria-hidden="true" onClick={() => handleShowMenu()}></i>
                 </div>
                 <ul className={showMenu? "show-menu": "menu"}>
                 {/* <ul className="show-menu"> */}
-                    <li>
-                        <NavLink to="/" className="link">Home</NavLink>
+                    <li >
+                        <NavLink to="/" className="link" onClick={() => handleShowDropdownMenu(true, "home")}>Home</NavLink>
                     </li>
                     <li>
-                        <NavLink to="/sessions" className="link">Sessions</NavLink>
+                        <div className="link pointer" onClick={() => handleShowDropdownMenu(!showDropdownMenu.show || true, "sessions")}>Sessions</div>
+                        {
+                        showDropdownMenu.show && showDropdownMenu.section === "sessions" ?
+                        <div className="dropdown-content">
+                            <NavLink to="/sessions/all" className="dropdown-link">All Sessions</NavLink>
+                            <NavLink to="/sessions/friends" className="dropdown-link">Friends sessions</NavLink>
+                        </div> 
+                        : null
+                        }
+                    </li>
+                    <li className="list-item">
+                        <div className="link pointer" onClick={() => handleShowDropdownMenu(true, "games")}>Games</div>
+                        {
+                        showDropdownMenu.show && showDropdownMenu.section === "games" ?
+                        <div className="dropdown-content">
+                            <NavLink to="/games/all" className="dropdown-link">All Games</NavLink>
+                            <NavLink to="/games/popular" className="dropdown-link">Popular Games</NavLink>
+                        </div> 
+                        : null
+                        }
                     </li>
                     <li>
-                        <NavLink to="/games" className="link">Games</NavLink>
-                    </li>
-                    <li>
-                        <NavLink to="/gameobjects" className="link">Game Objects</NavLink>
+                        <div className="link pointer" onClick={() => handleShowDropdownMenu(true, "gameobjects")}>Game Objects</div>
                     </li>
                 </ul>
             </div>
