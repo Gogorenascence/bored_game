7d3b1c72eead94054cdf3f58ee4c01a8467b80b6
Dean <omegadeangrey@gmail.com>
2023-11-15 01:09:24 -0500

    Audio file and button

--- ghi/src/MainPage.tsx
+++ ghi/src/MainPage.tsx
@@ -1,61 +1,61 @@
-import { NavLink } from "react-router-dom"
-import React, { useEffect, useState } from "react"
-import Carousel from "./Carousel"
-import { Game } from "./Games/GameInterface"
+import { NavLink } from "react-router-dom";
+import React, { useEffect, useState } from "react";
+import Carousel from "./Carousel";
+import { Game } from "./Games/GameInterface";
 
 function MainPage() {
-    
+  const [monthlyGames, setMonthlyGames] = useState([]);
+  const [dailyGames, setDailyGames] = useState([]);
+  const [topTenAllTimeGames, setTopTenAllTimeGames] = useState<Game[]>([]);
 
-    const [monthlyGames, setMonthlyGames] = useState([])
-    const [dailyGames, setDailyGames] = useState([])
-    const [topTenAllTimeGames, setTopTenAllTimeGames] = useState<Game[]>([])
+  const getGames = async () => {
+    const response = await fetch(
+      `${process.env.REACT_APP_API_HOST}/api/games/`
+    );
+    const gameData = await response.json();
+    setTopTenAllTimeGames(gameData.slice(0, 10));
+    setDailyGames(gameData);
+    setMonthlyGames(gameData);
+  };
 
-    const getGames = async() => {
-        const response = await fetch(`${process.env.REACT_APP_API_HOST}/api/games/`)
-        const gameData = await response.json()
-        setTopTenAllTimeGames(gameData.slice(0, 10))
-        setDailyGames(gameData)
-        setMonthlyGames(gameData)
-    }
+  useEffect(() => {
+    getGames();
+  }, []);
 
-    useEffect(() => {
-        getGames()
-    }, [])
+  console.log(topTenAllTimeGames);
 
-    console.log(topTenAllTimeGames)
-    
-    return (
-        <div className="main-content">
-            <h1 className="section-title">Most Popular Games</h1>
-            <div className="three-container">
-                <div>
-                    <h3 className="game-label">All Time</h3>
-                    <div className="game-window-1">
-                        <Carousel games={topTenAllTimeGames}/>
-                    </div>
-                </div>
-                <div>
-                    <h3 className="game-label">Monthly</h3>
-                    <div className="game-window-1">
-                        <p>{topTenAllTimeGames[0]?.name}</p>
-                    </div>
-                </div>
-                <div>
-                    <h3 className="game-label">Daily</h3>
-                    <div className="game-window-1">
-                        <p>{topTenAllTimeGames[0]?.name}</p>
-                    </div>
-                </div>
-            </div>
-            <div className="featured-container">
-                <div>
-                    <h1 className="section-title">Featured Games</h1>
-                    <div className="featured-games">
-                        <p>{topTenAllTimeGames[0]?.name}</p>
-                    </div>
-                </div>
-            </div>
+  return (
+    <div className="main-content">
+      <h1 className="section-title">Most Popular Games</h1>
+      <div className="three-container">
+        <div>
+          <h3 className="game-label">All Time</h3>
+          <div className="game-window-1">
+            <Carousel games={topTenAllTimeGames} />
+          </div>
         </div>
-    )
+        <div>
+          <h3 className="game-label">Monthly</h3>
+          <div className="game-window-1">
+            <p>{topTenAllTimeGames[0]?.name}</p>
+          </div>
+        </div>
+        <div>
+          <h3 className="game-label">Daily</h3>
+          <div className="game-window-1">
+            <p>{topTenAllTimeGames[0]?.name}</p>
+          </div>
+        </div>
+      </div>
+      <div className="featured-container">
+        <div>
+          <h1 className="section-title">Featured Games</h1>
+          <div className="featured-games">
+            <p>{topTenAllTimeGames[0]?.name}</p>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
 }
-export default MainPage
+export default MainPage;
--- ghi/src/Navbar.tsx
+++ ghi/src/Navbar.tsx
@@ -1,110 +1,164 @@
-import React, { useEffect, useState, useRef} from "react";
+import React, { useEffect, useState, useRef } from "react";
 import { NavLink } from "react-router-dom";
-
+// import audioFile from "./Audio/Home.mp3";
 
 function Navbar() {
+  interface dropDownMenu {
+    show: boolean;
+    section: string;
+  }
 
-    interface dropDownMenu {
-        show: boolean;
-        section: string
-    }
-    
-    const navBar = useRef<HTMLDivElement>(null);
+  const audioFile = require("./Audio/Home.mp3");
 
-    useOutsideAlerter(navBar)
+  const navBar = useRef<HTMLDivElement>(null);
+  const audioRef = useRef<HTMLAudioElement>(null);
 
-    const [showMenu, setShowMenu] = useState(false);
-    const [showDropdownMenu, setShowDropdownMenu] = useState<dropDownMenu>({
-        show: false,
-        section: ""
-     });
+  useOutsideAlerter(navBar);
 
-    const handleShowMenu = () => {
-        setShowMenu(!showMenu);
-    }
+  const [showMenu, setShowMenu] = useState(false);
+  const [showDropdownMenu, setShowDropdownMenu] = useState<dropDownMenu>({
+    show: false,
+    section: "",
+  });
 
-    const handleShowDropdownMenu = (show: boolean, section: string) => {
-        setShowDropdownMenu({show: show, section: section});
-    }
+  const [isAudioPlaying, setIsAudioPlaying] = useState(false);
 
-    function useOutsideAlerter(ref: React.RefObject<HTMLDivElement | null>) {
-        useEffect(() => {
-          // Function for click event
-          function handleOutsideClick(event: MouseEvent) {
-            if (ref.current && !ref.current.contains(event.target as Node)) {
-              handleShowDropdownMenu(false, "none");
-            }
-          }
-          // Adding click event listener
-          document.addEventListener("click", handleOutsideClick);
-          return () => document.removeEventListener("click", handleOutsideClick);
-        }, [ref]);
-      }
+  const handleShowMenu = () => {
+    setShowMenu(!showMenu);
+  };
 
+  const handleShowDropdownMenu = (show: boolean, section: string) => {
+    setShowDropdownMenu({ show: show, section: section });
+  };
+
+  function useOutsideAlerter(ref: React.RefObject<HTMLDivElement | null>) {
     useEffect(() => {
-        function handleResize() {
-          if (window.innerWidth > 800) {
-            // If window size is >= 800px and menu is open, close the menu
-            setShowMenu(false);
-          }
+      // Function for click event
+      function handleOutsideClick(event: MouseEvent) {
+        if (ref.current && !ref.current.contains(event.target as Node)) {
+          handleShowDropdownMenu(false, "none");
         }
-    
-        window.addEventListener('resize', handleResize);
-    
-        return () => {
-          window.removeEventListener('resize', handleResize);
-        };
-      }, [showMenu]);
+      }
+      // Adding click event listener
+      document.addEventListener("click", handleOutsideClick);
+      return () => document.removeEventListener("click", handleOutsideClick);
+    }, [ref]);
+  }
 
-    return (
-        <nav className="navbar" ref={navBar}>
-            <div className="navmain">
-                <div className="nav-header">
-                    <img src="bored-game-logo.svg" alt="logo"/>
-                    <i className="fa fa-bars" aria-hidden="true" onClick={() => handleShowMenu()}></i>
-                </div>
-                <ul className={showMenu ? "show-menu" : "menu"}>
-                {/* <ul className="show-menu"> */}
-                    <li >
-                        <NavLink to="/" className="link" onClick={() => handleShowDropdownMenu(true, "home")}>Home</NavLink>
-                    </li>
-                    <li>
-                        <div className="link pointer" onClick={() => handleShowDropdownMenu(!showDropdownMenu.show || true, "sessions")}>Sessions</div>
-                        {
-                        showDropdownMenu.show && showDropdownMenu.section === "sessions" ?
-                        <div className="dropdown-content">
-                            <NavLink to="/sessions/all" className="dropdown-link">All Sessions</NavLink>
-                            <NavLink to="/sessions/friends" className="dropdown-link">Friends sessions</NavLink>
-                        </div> 
-                        : null
-                        }
-                    </li>
-                    <li className="list-item">
-                        <div className="link pointer" onClick={() => handleShowDropdownMenu(true, "games")}>Games</div>
-                        {
-                        showDropdownMenu.show && showDropdownMenu.section === "games" ?
-                        <div className="dropdown-content">
-                            <NavLink to="/games/1" className="dropdown-link">All Games</NavLink>
-                            <NavLink to="/games/popular" className="dropdown-link">Popular Games</NavLink>
-                        </div> 
-                        : null
-                        }
-                    </li>
-                    <li>
-                        <div className="link pointer" onClick={() => handleShowDropdownMenu(true, "gameobjects")}>Game Objects</div>
-                    </li>
-                    <li className="user-log-signin">
-                        <div className="link pointer">Log In</div>
-                        <div className="link pointer">Sign Up</div>
-                    </li>
-                </ul>
-                <div className="nav-right">
-                    <p className="nav-button">Log In</p>
-                    <p className="nav-button">Sign Up</p>
-                </div>
+  const handlePlayClick = () => {
+    if (audioRef.current) {
+      if (isAudioPlaying) {
+        audioRef.current.pause();
+      } else {
+        audioRef.current.play();
+      }
+      setIsAudioPlaying(!isAudioPlaying);
+    }
+  };
+
+  useEffect(() => {
+    function handleResize() {
+      if (window.innerWidth > 800) {
+        // If window size is >= 800px and menu is open, close the menu
+        setShowMenu(false);
+      }
+    }
+
+    window.addEventListener("resize", handleResize);
+
+    return () => {
+      window.removeEventListener("resize", handleResize);
+    };
+  }, [showMenu]);
+
+  return (
+    <nav className="navbar" ref={navBar}>
+      <div className="navmain">
+        <div className="nav-header">
+          <img src="bored-game-logo.svg" alt="logo" />
+          <i
+            className="fa fa-bars"
+            aria-hidden="true"
+            onClick={() => handleShowMenu()}
+          ></i>
+        </div>
+        <ul className={showMenu ? "show-menu" : "menu"}>
+          {/* <ul className="show-menu"> */}
+          <li>
+            <NavLink
+              to="/"
+              className="link"
+              onClick={() => handleShowDropdownMenu(true, "home")}
+            >
+              Home
+            </NavLink>
+          </li>
+          <li>
+            <div
+              className="link pointer"
+              onClick={() =>
+                handleShowDropdownMenu(
+                  !showDropdownMenu.show || true,
+                  "sessions"
+                )
+              }
+            >
+              Sessions
             </div>
-        </nav>
-    )
+            {showDropdownMenu.show &&
+            showDropdownMenu.section === "sessions" ? (
+              <div className="dropdown-content">
+                <NavLink to="/sessions/all" className="dropdown-link">
+                  All Sessions
+                </NavLink>
+                <NavLink to="/sessions/friends" className="dropdown-link">
+                  Friends sessions
+                </NavLink>
+              </div>
+            ) : null}
+          </li>
+          <li className="list-item">
+            <div
+              className="link pointer"
+              onClick={() => handleShowDropdownMenu(true, "games")}
+            >
+              Games
+            </div>
+            {showDropdownMenu.show && showDropdownMenu.section === "games" ? (
+              <div className="dropdown-content">
+                <NavLink to="/games/1" className="dropdown-link">
+                  All Games
+                </NavLink>
+                <NavLink to="/games/popular" className="dropdown-link">
+                  Popular Games
+                </NavLink>
+              </div>
+            ) : null}
+          </li>
+          <li>
+            <div
+              className="link pointer"
+              onClick={() => handleShowDropdownMenu(true, "gameobjects")}
+            >
+              Game Objects
+            </div>
+          </li>
+          <button className="play-button" onClick={() => handlePlayClick()}>
+            {isAudioPlaying ? "Pause" : "AMBIENCE"}
+          </button>
+          <audio ref={audioRef} src={audioFile}></audio>
+          <li className="user-log-signin">
+            <div className="link pointer">Log In</div>
+            <div className="link pointer">Sign Up</div>
+          </li>
+        </ul>
+        <div className="nav-right">
+          <p className="nav-button">Log In</p>
+          <p className="nav-button">Sign Up</p>
+        </div>
+      </div>
+    </nav>
+  );
 }
 
 export default Navbar;
